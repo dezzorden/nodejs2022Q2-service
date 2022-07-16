252baeb525a78ec7f88fa847c456cfe93099d0d0
webdev <dezzzh93@gmail.com>
2022-07-16 17:52:52 +0800

    feat: add env file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule.initialize(), { cors: true });
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Сервер Nest')
     .setDescription('Документация по REST API')
@@ -12,6 +14,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
+
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT, () => console.log('Server started on port: ' + PORT));
 }
