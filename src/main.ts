8a9bbd3b9a9feaa2ce772498a58f462a56b9788e
webdev <dezzzh93@gmail.com>
2022-07-17 17:22:41 +0800

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.initialize(), { cors: true });
@@ -11,8 +14,11 @@ async function bootstrap() {
     .setDescription('Документация по REST API')
     .setVersion('0.0.1')
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
+  const API_DOCS = await readFile(
+    join(dirname(__dirname), 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const document = parse(API_DOCS);
   SwaggerModule.setup('/api/docs', app, document);
 
   const PORT = process.env.PORT || 5000;
